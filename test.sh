f4c717910061fe1e0095e4c2deb15a34b0c0f45e
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2018-03-10 19:51:58 +0100

    test: fix async testing, add more tests

--- test.sh
+++ test.sh
@@ -17,11 +17,12 @@ sync(){
   echo
 }
 async(){
-  task="$(post "async/$1" "$2" | jq -r .id)"
-  get  "async/$1/$task/wait"
+  local p="$(echo "$1" | cut -d'?' -f1)"
+  local task="$(post "async/$1" "$2" | jq -r .id)"
+  get  "async/$p/$task/wait"
   echo
-  get  "async/$1/$task/poll" >&2
-  post "async/$1/$task/kill" >&2
+  get  "async/$p/$task/poll" >&2
+  post "async/$p/$task/kill" >&2
 }
 check(){
   local my="$(eval "$1")"
@@ -40,17 +41,16 @@ test_product(){
   check "async product '[1,2,3,4,5]'" 120
 }
 test_polynomial(){
-  check "sync  polynomial '[[1,2,3,4,5], 3]'" 547
-  check "sync 'polynomial?pure=1' '[[1,2,3,4,5], 3]'" 547
-  check "sync 'polynomial?sum=async' '[[1,2,3,4,5], 3]'" 547
-  check "sync 'polynomial?product=async' '[[1,2,3,4,5], 3]'" 547
-  check "sync 'polynomial?sum=async&product=async' '[[1,2,3,4,5], 3]'" 547
-  check "async polynomial '[[1,2,3,4,5], 3]'" 547
-# TODO
-# check "async 'polynomial?pure=1' '[[1,2,3,4,5], 3]'" 547
-# check "async 'polynomial?sum=async' '[[1,2,3,4,5], 3]'" 547
-# check "async 'polynomial?product=async' '[[1,2,3,4,5], 3]'" 547
-# check "async 'polynomial?sum=async&product=async' '[[1,2,3,4,5], 3]'" 547
+  check "sync   polynomial '$1'" "$2"
+  check "sync  'polynomial?pure=1' '$1'" "$2"
+  check "sync  'polynomial?sum=async' '$1'" "$2"
+  check "sync  'polynomial?product=async' '$1'" "$2"
+  check "sync  'polynomial?sum=async&product=async' '$1'" "$2"
+  check "async  polynomial '$1'" "$2"
+  check "async 'polynomial?pure=1' '$1'" "$2"
+  check "async 'polynomial?sum=async' '$1'" "$2"
+  check "async 'polynomial?product=async' '$1'" "$2"
+  check "async 'polynomial?sum=async&product=async' '$1'" "$2"
 }
 
 if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
@@ -58,5 +58,6 @@ if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
   exec 2>/dev/null
   test_sum
   test_product
-  test_polynomial
+  test_polynomial '[[1,2], 3]' 7
+  test_polynomial '[[1,2,3,4,5], 3]' 547
 fi
